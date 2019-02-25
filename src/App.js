5796534009215cdc578f7f28910edd2965416f87
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-25 13:42:29 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  state = {
+      peeps: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(({people}) => this.setState({ peeps: people }))
+  }
+
+  render() {
+   return (
+     <div>
+       {this.state.peeps.map((person) => <h1>{person.name}</h1>)}
+     </div>
+   )
+ }
+
+}
+
+export default App
